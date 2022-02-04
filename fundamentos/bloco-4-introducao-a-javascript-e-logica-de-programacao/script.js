b9ba507a530910f9a048e6892cfcd84f67858367
jean <jeansantos@id.uff.br>
2022-02-03 21:23:05 -0300

    primeiro ex resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -0,0 +1,14 @@
+const a = 10;
+const b = 7;
+
+// let adicao = a + b;
+// console.log(adicao);
+// let sub = a - b;
+//  console.log(sub);
+// let mult = a * b;
+// console.log(mult);
+// let div = a / b;
+// console.log(div);
+let resto = a % b;
+console.log(resto);
+
