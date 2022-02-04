5bb24b93a803cf150b393e612c0b470529d41170
jean <jeansantos@id.uff.br>
2022-02-03 21:54:45 -0300

    exercicio 2 e 3 resolvidos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/script.js
@@ -1,6 +1,8 @@
 const a = 10;
 const b = 7;
+const c = 5;
 
+// 1)
 // let adicao = a + b;
 // console.log(adicao);
 // let sub = a - b;
@@ -9,6 +11,27 @@ const b = 7;
 // console.log(mult);
 // let div = a / b;
 // console.log(div);
-let resto = a % b;
-console.log(resto);
+// let resto = a % b;
+// console.log(resto);
+
+// 2)
+// if (a > b) {
+//     console.log(a)
+// }
+
+if (a > b && a > c ) {
+    console.log(a);
+} else if (b > a && b > c ) {
+    console.log(b);
+} else  {
+    console.log(c);
+}
+
+
+
+
+
+
+
+
 
